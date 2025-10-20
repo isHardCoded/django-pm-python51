d023b19766061289d5501427a043b48e5628b2eb
isHardCoded <ishardcoded@gmail.com>
2025-10-20 21:45:24 +0500

    Добавили данные в БД, написали вьюшку для получения проектов для канбан-доски

--- projects/models.py
+++ projects/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class ProjectStatus(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
