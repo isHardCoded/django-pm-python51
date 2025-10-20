e4436c55f6b430e409f764e060fc2c4bd0ec9170
isHardCoded <ishardcoded@gmail.com>
2025-10-20 20:50:56 +0500

    Создали приложение для проектов, сделали модели и миграции

---
+++ projects/models.py
@@ -0,0 +1,14 @@
+from users.models import CustomUser
+from django.db import models
+
+class ProjectStatus(models.Model):
+    name = models.CharField(max_length=100)
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.ForeignKey(ProjectStatus, on_delete=models.CASCADE)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
