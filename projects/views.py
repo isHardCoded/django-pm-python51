2e7b5f895ebc3be87d7f1f042f425e1140ab377f
isHardCoded <ishardcoded@gmail.com>
2025-10-27 19:38:07 +0300

    Реализовали добавление проекта

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from projects.models import ProjectStatus, Project
 
@@ -12,3 +12,35 @@ def kanban(request):
     }
 
     return render(request, 'projects/kanban.html', context)
+
+def add(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        description = request.POST["description"]
+        status = request.POST["status"]
+        date_start = request.POST["date_start"]
+        date_end = request.POST["date_end"]
+
+        if title and description and status and date_start and date_end:
+            if not Project.objects.filter(title=title).exists():
+                Project.objects.create(
+                    title=title,
+                    description=description,
+                    status_id=status,
+                    owner_id=request.user.id,
+                    date_start=date_start,
+                    date_end=date_end)
+
+                return redirect("projects_kanban")
+            else:
+                error = "Проект с таким названием уже существует"
+        else:
+            error = "Заполните все поля"
+
+        return render(request, "projects/add-form.html", { "error": error })
+
+    context = {
+        "statuses": ProjectStatus.objects.all()
+    }
+
+    return render(request, "projects/add-form.html", context)
