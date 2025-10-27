0b57389db4324754a9c5200ee502b01a5306aed3
isHardCoded <ishardcoded@gmail.com>
2025-10-27 18:45:14 +0300

    Сверстали страницу с проектами

--- projects/views.py
+++ projects/views.py
@@ -7,8 +7,8 @@ def kanban(request):
     context = {
         "status_planning": ProjectStatus.objects.get(name="Планируется"),
         "status_progress": ProjectStatus.objects.get(name="Выполняется"),
-        "projects_planning": Project.objects.get(status=ProjectStatus.objects.get(name="Планируется")),
-        "projects_progress": Project.objects.get(status=ProjectStatus.objects.get(name="Выполняется")),
+        "projects_planning": Project.objects.filter(status=ProjectStatus.objects.get(name="Планируется")),
+        "projects_progress": Project.objects.filter(status=ProjectStatus.objects.get(name="Выполняется")),
     }
 
     return render(request, 'projects/kanban.html', context)
