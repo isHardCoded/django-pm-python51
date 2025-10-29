4210ffa2f5b539d90c1942ad9057c89609ab44ce
isHardCoded <ishardcoded@gmail.com>
2025-10-29 18:16:10 +0300

    Добавили возможность выхода из аккаунта

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from users.views import *
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
 ]
