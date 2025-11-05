9d9c9464065b98905fad0fc66b4dfdfc9c22b6b1
isHardCoded <ishardcoded@gmail.com>
2025-11-05 18:22:09 +0300

    Реализовали форму создания задачи

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,5 @@ from tasks.views import *
 
 urlpatterns = [
     path('kanban/', kanban, name='tasks_kanban'),
+    path('add/', add, name='tasks_add'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from users.models import CustomUser
+from projects.models import Project
 from tasks.models import TaskStatus, Task
 
 @login_required(login_url='login')
@@ -13,3 +15,41 @@ def kanban(request):
     }
 
     return render(request, 'tasks/kanban.html', context)
+
+@login_required(login_url='login')
+def add(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        description = request.POST["description"]
+        status = request.POST["status"]
+        assign = request.POST["assign"]
+        project = request.POST["project"]
+        deadline = request.POST["deadline"]
+
+        if title and description and status and assign and project and deadline:
+            if not Task.objects.filter(title=title).exists():
+                Task.objects.create(
+                    title=title,
+                    description=description,
+                    status_id=status,
+                    owner_id=request.user.id,
+                    assign_id=assign,
+                    project_id=project,
+                    deadline=deadline,
+                )
+
+                return redirect("tasks_kanban")
+            else:
+                error = "Задача с таким названием уже существует"
+        else:
+            error = "Заполните все поля"
+
+        return render(request, "tasks/add-form.html", { "error": error })
+
+    context = {
+        "statuses": TaskStatus.objects.all(),
+        "users": CustomUser.objects.all(),
+        "projects": Project.objects.all(),
+    }
+
+    return render(request, "tasks/add-form.html", context)
