916aaa183348be8a93eab04c827fcf7d72185afd
isHardCoded <ishardcoded@gmail.com>
2025-12-01 19:39:36 +0300

    Сделали отображение проектов в виде таблицы

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from tasks.views import *
 
 urlpatterns = [
     path('kanban/', kanban, name='tasks_kanban'),
+    path('table/', table, name='tasks_table'),
     path('add/', add, name='tasks_add'),
 ]
